e7e9626b3195e711717ae782b685c8eab30812c0
Matheus Kenzo <100180936+Dobat00@users.noreply.github.com>
2024-01-04 20:40:30 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaRestaurante/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaRestaurante.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
